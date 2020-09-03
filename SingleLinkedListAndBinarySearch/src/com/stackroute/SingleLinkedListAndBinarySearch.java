c15c692fba9dfb27dc4f74f82a447882982e4e82
Sushma Bhupathi <bhupathisushma1998@gmail.com>
2020-09-03 05:30:14 +0000

    Added single linked list and binary search program

---
+++ SingleLinkedListAndBinarySearch/src/com/stackroute/SingleLinkedListAndBinarySearch.java
@@ -0,0 +1,107 @@
+package com.stackroute;
+
+public class SingleLinkedListAndBinarySearch {
+
+
+}
+// Java code to implement binary search
+// on Singly Linked List
+class Node
+{
+    int data;
+    Node next;
+
+    // Constructor to create a new node
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+
+class BinarySearch
+{
+    // function to insert a node at the beginning
+    // of the Singaly Linked List
+    static Node push(Node head, int data)
+    {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+        return head;
+    }
+
+    // Function to find middle element
+    // using Fast and Slow pointers
+    static Node middleNode(Node start, Node last)
+    {
+        if (start == null)
+            return null;
+
+        Node slow = start;
+        Node fast = start.next;
+
+        while (fast != last)
+        {
+            fast = fast.next;
+            if (fast != last)
+            {
+                slow = slow.next;
+                fast = fast.next;
+            }
+        }
+        return slow;
+    }
+
+    // function to insert a node at the beginning
+    // of the Singly Linked List
+    static Node binarySearch(Node head, int value)
+    {
+        Node start = head;
+        Node last = null;
+
+        do
+        {
+            Node mid = middleNode(start, last);
+
+            if (mid == null)
+                return null;
+
+            if (mid.data == value)
+                return mid;
+
+            else if (mid.data > value)
+            {
+                start = mid.next;
+            }
+
+            else
+                last = mid;
+        } while (last == null || last != start);
+
+        return null;
+    }
+
+    public static void main(String[] args)
+    {
+        Node head = null;
+
+
+        head = push(head, 1);
+        head = push(head, 4);
+        head = push(head, 7);
+        head = push(head, 8);
+        head = push(head, 9);
+        head = push(head, 10);
+        int value = 73;
+
+        if (binarySearch(head, value) == null)
+        {
+            System.out.println("Value not present");
+        }
+        else
+        {
+            System.out.println("Present");
+        }
+    }
+}
