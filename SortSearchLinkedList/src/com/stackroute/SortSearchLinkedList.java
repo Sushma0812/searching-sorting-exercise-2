e0524c1ee8a858fd485f1d33c81c2cd318a406c6
Sushma Bhupathi <bhupathisushma1998@gmail.com>
2020-09-03 13:39:11 +0000

    Added .gitignore file

--- SortSearchLinkedList/src/com/stackroute/SortSearchLinkedList.java
+++ SortSearchLinkedList/src/com/stackroute/SortSearchLinkedList.java
@@ -1,4 +1,6 @@
 package com.stackroute;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Scanner;
 public class SortSearchLinkedList {
     public static String[] getDetails() {
@@ -11,27 +13,27 @@ public class SortSearchLinkedList {
         }
         return array;
     }
-    void bubbleSort(String[] arr,int n){
+    void bubbleSort(String[] array,int size){
         String temp;
 
         //sorting items using bubble sort
-        for (int i=0;i<n-1;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i].compareTo(arr[j])>0){
-                    temp=arr[j];
-                    arr[j]=arr[i];
-                    arr[i]=temp;
+        for (int i=0;i<size-1;i++){
+            for(int j=i+1;j<array.length;j++){
+                if(array[i].compareTo(array[j])>0){
+                    temp=array[j];
+                    array[j]=array[i];
+                    array[i]=temp;
                 }
             }
         }
-        for(int i=0;i<arr.length;i++){
-            System.out.println(i+":"+arr[i]);
+        for(int i=0;i<array.length;i++){
+            System.out.println(i+":"+array[i]);
         }
     }
-    piblic static String[] Add(String[] array){
-        Linkedlist<String> list = new LinkedList<String>();
+    public static String[] Add(String[] array){
+        Linkedlist<String> list = new Linkedlist<String>();
         for(int i=0;i<array.length;i++){
-            list.add(array[i]);
+            list.add(array[i];
         }
         System.out.println(Arrays.toString(array));
         return null;
@@ -59,31 +61,23 @@ public class SortSearchLinkedList {
     }
 
     public static void main(String[] args) {
-        String[] items = getDetails();
-        SortSearchLinkedList obj=new SortSearchLinkedList();
-        Scanner sc=new Scanner(System.in);
-        //String[] items=new String[5];
-
-        //taking items from user
-       /* System.out.println("Enter 5 items");
-        for(int i=0;i<items.length;i++){
-            items[i]=sc.next();
-        }*/
-
-        //printing items before sorting
-        for (int i=0;i<items.length;i++){
-            System.out.println(i+":"+items[i]);
+        String[] strings = getDetails();
+        SortSearchLinkedList searchLinkedList=new SortSearchLinkedList();
+        Scanner reader=new Scanner(System.in);
+        //printing strings before sorting
+        for (int i=0;i<strings.length;i++){
+            System.out.println(i+":"+strings[i]);
         }
-        Add(items);
+        Add(strings);
+        searchLinkedList.bubbleSort(strings,strings.length);
 
-        obj.bubbleSort(items,items.length);
-
-        //accepting the search element
+        //accepting the key element
         System.out.println("Enter the item to be searched");
-        String search=sc.next();
-        obj.search(items,search);
+        String key=reader.next();
+        searchLinkedList.search(strings,key);
     }
+
+
 }
-//SortItem.java
-//Displaying SortItems.java.
+
 
